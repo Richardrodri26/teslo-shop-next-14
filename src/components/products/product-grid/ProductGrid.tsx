72927589d00540a1878438b3c1f59bc5971bd4a9
Richard Rodriguez <richardmanuel811@gmail.com>
2024-02-29 21:04:20 -0500

    [UP] ProductItem component add new dependencies add sidebart add ui store Finishing section 14

--- src/components/products/product-grid/ProductGrid.tsx
+++ src/components/products/product-grid/ProductGrid.tsx
@@ -1,5 +1,6 @@
 import { Product } from '@/interfaces'
 import React from 'react'
+import { ProductGridItem } from './ProductGridItem';
 
 interface Props {
   products: Product[];
@@ -10,7 +11,7 @@ export const ProductGrid = ({ products }: Props) => {
     <div className='grid grid-cols-2 sm:grid-cols-3 gap-10 mb-10'>
       {
         products.map(product => (
-          <span key={product.slug}>{product.title}</span>
+          <ProductGridItem product={product} key={product.slug} />
         ))
       }
     </div>
