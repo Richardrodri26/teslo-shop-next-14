65c7916825b4d16d8268452cd60a7ccb683f94f7
RichardRodri26 <richardmanuel811@gmail.com>
2024-11-26 20:55:49 -0500

    working on section 17

---
+++ src/actions/product/product-pagination.ts
@@ -0,0 +1,36 @@
+'use server';
+
+import prisma from "@/lib/prisma";
+
+
+export const getPaginatedProductsWithImages = async () => {
+
+  try {
+    const products = await prisma.product.findMany({
+      take: 12,
+      include: {
+        ProductImage: {
+          take: 2,
+          select: {
+            url: true,
+          }
+        }
+      }
+    })
+
+
+
+    return {
+      currentPage: 1,
+      totalPages: 10,
+      products: products.map(product => ({
+        ...product,
+        images: product.ProductImage.map(image => image.url)
+      }))
+    };
+
+  } catch (error) {
+    throw new Error('No se pudo cargar los productos')
+  }
+
+}
--- src/interfaces/product.interface.ts
+++ src/interfaces/product.interface.ts
@@ -1,6 +1,6 @@
 
 export interface Product {
-  // TODO id: string;
+  id: string;
   description: string;
   images: string[];
   inStock: number;
@@ -9,7 +9,7 @@ export interface Product {
   slug: string;
   tags: string[];
   title: string;
-  type: Type;
+  // todo: type: Type;
   gender: Category
 }
 
